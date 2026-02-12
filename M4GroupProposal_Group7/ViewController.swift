42206db114df8370675f418ff9254fa397a55c9c
XJM <xander_moore1@mymail.eku.edu>
2026-02-12 12:01:27 -0500

    Initial Commit

---
+++ M4GroupProposal_Group7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M4GroupProposal_Group7
+//
+//  Created by Moore, Xander J. on 2/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
